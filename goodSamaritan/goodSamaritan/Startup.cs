cc104ec2fd2a36907249c3c2d81b058e29b46e8c
Huck5 <fcharleslundaahl@gmail.com>
2015-02-19 11:47:24 -0800

    New Solution

---
+++ goodSamaritan/goodSamaritan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(goodSamaritan.Startup))]
+namespace goodSamaritan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
