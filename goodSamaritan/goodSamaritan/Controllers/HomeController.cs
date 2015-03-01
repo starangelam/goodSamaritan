bb473296fb90c17eb5108972ffe40867d0be2528
Angela M <starangelam@gmail.com>
2015-03-01 12:06:51 -0800

    UI changes: convert default to a fake Good Samaritan website

--- goodSamaritan/goodSamaritan/Controllers/HomeController.cs
+++ goodSamaritan/goodSamaritan/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace GoodSamaritan.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Mission, Vision, Values.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "CORPORATE OFFICE";
 
             return View();
         }
