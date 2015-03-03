77654caba3f48f6ee20ce424c685d9c39fc4445e
Angela M <starangelam@gmail.com>
2015-03-02 20:47:18 -0800

    seed users with different roles

---
+++ goodSamaritan/goodSamaritan/Migrations/IdentityMigration/Configuration.cs
@@ -0,0 +1,62 @@
+namespace GoodSamaritan.Migrations.IdentityMigration
+{
+    using GoodSamaritan.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GoodSamaritan.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\IdentityMigration";
+        }
+
+        protected override void Seed(GoodSamaritan.Models.ApplicationDbContext context)
+        {
+            // create roles: administrator, worker, report
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Administrator"))
+                roleManager.Create(new IdentityRole("Administrator"));
+
+            if (!roleManager.RoleExists("Worker"))
+                roleManager.Create(new IdentityRole("Worker"));
+
+            if (!roleManager.RoleExists("Report"))
+                roleManager.Create(new IdentityRole("Report"));
+
+            // create users
+            string email;
+            string password = "P@$$w0rd";
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            email = "adam@gs.ca";
+            if (!(context.Users.Any(u => u.UserName == email)))
+            {
+                ApplicationUser userToInsert = new ApplicationUser { UserName = email, Email = email };
+                userManager.Create(userToInsert, password);
+                userManager.AddToRole(userToInsert.Id, "Administrator");
+            }
+
+            email = "wendy@gs.ca";
+            if (!(context.Users.Any(u => u.UserName == email)))
+            {
+                ApplicationUser userToInsert = new ApplicationUser { UserName = email, Email = email };
+                userManager.Create(userToInsert, password);
+                userManager.AddToRole(userToInsert.Id, "Worker");
+            }
+
+            email = "rob@gs.ca";
+            if (!(context.Users.Any(u => u.UserName == email)))
+            {
+                ApplicationUser userToInsert = new ApplicationUser { UserName = email, Email = email };
+                userManager.Create(userToInsert, password);
+                userManager.AddToRole(userToInsert.Id, "Report");
+            }
+        }
+    }
+}
