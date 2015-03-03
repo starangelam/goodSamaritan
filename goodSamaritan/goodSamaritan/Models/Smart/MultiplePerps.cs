105736597421bc0f4f415140d3f99e140155a3b2
Huck5 <fcharleslundaahl@gmail.com>
2015-03-02 22:50:22 -0800

    Adding Smart entity and lookups

---
+++ goodSamaritan/goodSamaritan/Models/Smart/MultiplePerps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoodSamaritan.Models.Smart
+{
+    public class MultiplePerps
+    {
+        public int MultiplePerpsId { get; set; }
+        public string Value { get; set; }
+        public ICollection<Smart> Smarts { get; set; }
+    }
+}
---
+++ goodSamaritan/goodSamaritan/Models/Smart/Smart.cs
@@ -0,0 +1,26 @@
+﻿using goodSamaritan.Models.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoodSamaritan.Models.Smart
+{
+    public class Smart
+    {
+        public int SmartId { get; set; }
+
+        // FK to Client
+        public Client Client { get; set; }
+        public int ClientId { get; set; }
+
+        public SexExploitation SexExploitation { get; set; }
+        public int SexExploitationId { get; set; }
+
+        public MultiplePerps MultiplePerps { get; set; }
+        public int MultiplePerpsId { get; set; }
+
+        public DrugFacilitated DrugFacilitated { get; set; }
+        public int DrugFacilitatedId { get; set; }
+    }
+}
