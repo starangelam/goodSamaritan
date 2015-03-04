d6e5bfc35c52269316cc9358463cacd19ca8bc16
Angela M <starangelam@gmail.com>
2015-03-04 13:18:40 -0800

    List all users for admin to manage

---
+++ goodSamaritan/goodSamaritan/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using GoodSamaritan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoodSamaritan.Controllers
+{
+    [Authorize(Roles="Administrator")]
+    public class UsersController : Controller
+    {
+        ApplicationDbContext ctx = new ApplicationDbContext();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = ctx.Users.ToList();
+            return View(users);
+        }
+
+        // GET: Users/Disable/5
+        public ActionResult Disable(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = ctx.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Disable/5
+        [HttpPost, ActionName("Disable")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DisableConfirmed(string id)
+        {
+            //AccountController account = new AccountController();
+            //account.UserManager.RemovePasswordAsync(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
