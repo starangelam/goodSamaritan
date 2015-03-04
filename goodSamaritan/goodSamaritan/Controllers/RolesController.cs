fb87cab7a59909eec10455f349c968bc14627ca6
Angela M <starangelam@gmail.com>
2015-03-03 21:04:23 -0800

    list, add and delete roles

---
+++ goodSamaritan/goodSamaritan/Controllers/RolesController.cs
@@ -0,0 +1,77 @@
+﻿using GoodSamaritan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoodSamaritan.Controllers
+{
+    public class RolesController : Controller
+    {
+        ApplicationDbContext ctx = new ApplicationDbContext();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            var roles = ctx.Roles.ToList();
+            return View(roles);
+        }
+
+        // GET: /Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Roles/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                ctx.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
+                {
+                    Name = collection["RoleName"]
+                });
+
+                ctx.SaveChanges();
+
+                ViewBag.ResultMessage = "Role created successfully !";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = ctx.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            var role = ctx.Roles.Find(id);
+            ctx.Roles.Remove(role);
+            ctx.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
