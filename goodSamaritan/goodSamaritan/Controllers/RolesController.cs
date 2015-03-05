ad6661e597c7c19b1651b4a6feb53b32bd6691b9
Angela M <starangelam@gmail.com>
2015-03-04 23:30:19 -0800

    add and remove user from role

--- goodSamaritan/goodSamaritan/Controllers/RolesController.cs
+++ goodSamaritan/goodSamaritan/Controllers/RolesController.cs
@@ -1,4 +1,6 @@
 ﻿using GoodSamaritan.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,28 @@ using System.Web.Mvc;
 
 namespace GoodSamaritan.Controllers
 {
-    [Authorize(Roles="Aministrator")]
+    [Authorize(Roles = "Administrator")]
     public class RolesController : Controller
     {
         ApplicationDbContext ctx = new ApplicationDbContext();
+        UserManager<ApplicationUser> _userManager;
+
+        public UserManager<ApplicationUser> UserManager
+        {
+            get
+            {
+                if (_userManager == null)
+                {
+                    var userStore = new UserStore<ApplicationUser>(ctx);
+                    _userManager = new UserManager<ApplicationUser>(userStore);
+                }
+                return _userManager;
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
 
         // GET: Roles
         public ActionResult Index()
@@ -73,6 +93,108 @@ namespace GoodSamaritan.Controllers
             ctx.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+        // GET: ManageUserRoles
+        public ActionResult ManageUserRoles()
+        {
+            ViewBag.Roles = from r in ctx.Roles
+                            orderby r.Name
+                            select new SelectListItem { Value = r.Id, Text = r.Name };
+
+            ViewBag.Users = from u in ctx.Users
+                            orderby u.UserName
+                            select new SelectListItem { Value = u.Id, Text = u.UserName };
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ShowUserRoles(string userId)
+        {
+            if (userId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = ctx.Users.Find(userId);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.UserName = user.UserName;
+            ViewBag.Roles = UserManager.GetRoles(userId);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddUserToRole(string roleId, string userId)
+        {
+            if (roleId == null || userId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = ctx.Users.Find(userId);
+            var role = ctx.Roles.Find(roleId);
+            if (user == null || role == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (UserManager.IsInRole(userId, role.Name))
+            {
+                ViewBag.ErrorMsg = String.Format("This user already belong to selected role \"{0}\".",
+                    role.Name);
+
+            }
+            else
+            {
+                UserManager.AddToRole(userId, role.Name);
+
+                ViewBag.SuccessMsg = String.Format("Role \"{0}\" added to this user successfully!",
+                    role.Name);
+            }
+            ViewBag.UserName = user.UserName;
+            ViewBag.Roles = UserManager.GetRoles(userId);
+
+            return View("ShowUserRoles");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RemoveUserFromRole(string roleId, string userId)
+        {
+            if (roleId == null || userId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = ctx.Users.Find(userId);
+            var role = ctx.Roles.Find(roleId);
+            if (user == null || role == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (UserManager.IsInRole(userId, role.Name))
+            {
+                UserManager.RemoveFromRole(userId, role.Name);
+                ViewBag.SuccessMsg = String.Format("Role \"{0}\" removed from this user successfully!", 
+                    role.Name);
+            }
+            else
+            {
+                ViewBag.ErrorMsg = String.Format("This user doesn't belong to selected role \"{0}\".", 
+                    role.Name);
+            }
+
+            ViewBag.UserName = user.UserName;
+            ViewBag.Roles = UserManager.GetRoles(userId);
+
+            return View("ShowUserRoles");
         }
     }
 }
