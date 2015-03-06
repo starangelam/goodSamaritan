e05156817b3794eb7bef038c7beb4666836bb433
Angela M <starangelam@gmail.com>
2015-03-05 19:46:29 -0800

    add Required annotation to Smart entity

---
+++ goodSamaritan/goodSamaritan/Migrations/ClientMigrations/201503060345388_SmartAddRequired.cs
@@ -0,0 +1,16 @@
+namespace GoodSamaritan.Migrations.ClientMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SmartAddRequired : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- goodSamaritan/goodSamaritan/Models/Smart/Smart.cs
+++ goodSamaritan/goodSamaritan/Models/Smart/Smart.cs
@@ -10,67 +10,76 @@ namespace GoodSamaritan.Models.Smart
 {
     public class Smart
     {
+        [Required]
         public int SmartId { get; set; }
 
         // FK to Client
-        public Client Client { get; set; }
+        [Required]
         public int ClientId { get; set; }
+        public Client Client { get; set; }
 
-        public SexExploitation SexExploitation { get; set; }
         public int SexExploitationId { get; set; }
+        public SexExploitation SexExploitation { get; set; }
 
-        public MultiplePerps MultiplePerps { get; set; }
         public int MultiplePerpsId { get; set; }
+        public MultiplePerps MultiplePerps { get; set; }
 
-        public DrugFacilitated DrugFacilitated { get; set; }
         public int DrugFacilitatedId { get; set; }
+        public DrugFacilitated DrugFacilitated { get; set; }
 
-        public CityOfAssault CityOfAssault { get; set; }
+        [Required]
         public int CityOfAssaultId { get; set; }
+        public CityOfAssault CityOfAssault { get; set; }
 
-        public CityOfResidence CityOfResidence { get; set; }
+        [Required]
         public int CityOfResidenceId { get; set; }
+        public CityOfResidence CityOfResidence { get; set; }
 
+        [Required]
         public int AccompanimentMinutes { get; set; }
 
-        public ReferringHospital ReferringHospital { get; set; }
+        [Required]
         public int ReferringHospitalId { get; set; }
+        public ReferringHospital ReferringHospital { get; set; }
 
-        public HospitalAttended HospitalAttended { get; set; }
+        [Required]
         public int HospitalAttendedId { get; set; }
+        public HospitalAttended HospitalAttended { get; set; }
 
-        public SocialWorkAttendance SocialWorkAttendance { get; set; }
         public int SocialWorkAttendanceId { get; set; }
+        public SocialWorkAttendance SocialWorkAttendance { get; set; }
 
-        public PoliceAttendance PoliceAttendance { get; set; }
         public int PoliceAttendanceId { get; set; }
+        public PoliceAttendance PoliceAttendance { get; set; }
 
-        public VictimServicesAttendance VictimServicesAttendance { get; set; }
         public int VictimServiceAttendanceId { get; set; }
+        public VictimServicesAttendance VictimServicesAttendance { get; set; }
 
-        public MedicalOnly MedicalOnly { get; set; }
         public int MedicalOnlyId { get; set; }
+        public MedicalOnly MedicalOnly { get; set; }
 
-        public EvidenceStored EvidenceStored { get; set; }
         public int EvidenceStoredId { get; set; }
+        public EvidenceStored EvidenceStored { get; set; }
 
-        public HivMeds HivMeds { get; set; }
         public int HivMedsId { get; set; }
+        public HivMeds HivMeds { get; set; }
 
-        public ReferredToCbvs ReferredToCbvs { get; set; }
         public int ReferredToCbvsId { get; set; }
+        public ReferredToCbvs ReferredToCbvs { get; set; }
 
-        public PoliceReported PoliceReported { get; set; }
         public int PoliceReportedId { get; set; }
+        public PoliceReported PoliceReported { get; set; }
 
-        public ThirdPartyReport ThirdPartyReport { get; set; }
         public int ThirdPartyReportId { get; set; }
+        public ThirdPartyReport ThirdPartyReport { get; set; }
 
-        public BadDateReport BadDateReport { get; set; }
         public int BadDateReportId { get; set; }
+        public BadDateReport BadDateReport { get; set; }
 
+        [Required]
         public int NumberTransportsProvided { get; set; }
 
+        [Required]
         public Boolean ReferredToNursePracticioner { get; set; }
     } 
 }
