63947bd2cb8032b920a9ba95b6248e9ebd82c62c
Angela M <starangelam@gmail.com>
2015-03-05 18:07:24 -0800

    include details for lookup table in client

--- goodSamaritan/goodSamaritan/Controllers/ClientsController.cs
+++ goodSamaritan/goodSamaritan/Controllers/ClientsController.cs
@@ -18,7 +18,25 @@ namespace GoodSamaritan.Controllers
         // GET: Clients
         public async Task<ActionResult> Index()
         {
-            var clients = db.Clients.Include(c => c.AbuserRelationship).Include(c => c.Age).Include(c => c.AssignedWorkder).Include(c => c.Crisis).Include(c => c.DuplicateFile).Include(c => c.Ethnicity).Include(c => c.FamilyViolenceFile).Include(c => c.FiscalYear).Include(c => c.Incident).Include(c => c.Program).Include(c => c.ReferralContact).Include(c => c.ReferralSource).Include(c => c.RepeatClient).Include(c => c.RiskLevel).Include(c => c.RiskStatus).Include(c => c.Service).Include(c => c.StatusOfFile).Include(c => c.VictimOfIncident);
+            var clients = db.Clients
+                .Include(c => c.AbuserRelationship)
+                .Include(c => c.Age)
+                .Include(c => c.AssignedWorkder)
+                .Include(c => c.Crisis)
+                .Include(c => c.DuplicateFile)
+                .Include(c => c.Ethnicity)
+                .Include(c => c.FamilyViolenceFile)
+                .Include(c => c.FiscalYear)
+                .Include(c => c.Incident)
+                .Include(c => c.Program)
+                .Include(c => c.ReferralContact)
+                .Include(c => c.ReferralSource)
+                .Include(c => c.RepeatClient)
+                .Include(c => c.RiskLevel)
+                .Include(c => c.RiskStatus)
+                .Include(c => c.Service)
+                .Include(c => c.StatusOfFile)
+                .Include(c => c.VictimOfIncident);
             return View(await clients.ToListAsync());
         }
 
@@ -29,7 +47,27 @@ namespace GoodSamaritan.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Client client = await db.Clients.FindAsync(id);
+            Client client = await db.Clients
+                .Include(c => c.AbuserRelationship)
+                .Include(c => c.Age)
+                .Include(c => c.AssignedWorkder)
+                .Include(c => c.Crisis)
+                .Include(c => c.DuplicateFile)
+                .Include(c => c.Ethnicity)
+                .Include(c => c.FamilyViolenceFile)
+                .Include(c => c.FiscalYear)
+                .Include(c => c.Incident)
+                .Include(c => c.Program)
+                .Include(c => c.ReferralContact)
+                .Include(c => c.ReferralSource)
+                .Include(c => c.RepeatClient)
+                .Include(c => c.RiskLevel)
+                .Include(c => c.RiskStatus)
+                .Include(c => c.Service)
+                .Include(c => c.StatusOfFile)
+                .Include(c => c.VictimOfIncident)
+                .Where(c => c.ClientId == id)
+                .SingleOrDefaultAsync();
             if (client == null)
             {
                 return HttpNotFound();
