f607dae220c477360210fb2ab984d2b91ef0ecb2
Angela M <starangelam@gmail.com>
2015-04-08 20:59:01 -0700

    create report

---
+++ goodSamaritan/goodSamaritan/Controllers/ReportController.cs
@@ -0,0 +1,44 @@
+﻿using goodSamaritan.Models.Client;
+using GoodSamaritan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoodSamaritan.Controllers
+{
+    [Authorize(Roles = "Administrator, Worker, Reporter")]
+    public class ReportController : Controller
+    {
+        ClientContext db = new ClientContext();
+        // GET: Report
+        public ActionResult Index()
+        {
+            // TODO if check on selected null, redirect
+            //int selectedWorker = db.AssignedWorkers.FirstOrDefault().AssignedWorkerId;
+            //int selectedStatus = db.StatusesOfFiles.Where( s => s.Status == "Open").FirstOrDefault().StatusOfFileId;
+            
+            ViewBag.Workers = new SelectList(db.AssignedWorkers, "AssignedWorkerId", "Name");
+            ViewBag.Status = new SelectList(db.StatusesOfFiles, "StatusOfFileId", "Status");
+
+            //Report report = new Report(selectedWorker, selectedStatus);
+
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Report")]
+        public ActionResult GenerateReport(FormCollection reportFilters)
+        {
+            ViewBag.ReportDate = DateTime.Now.ToString("yyyy-MM-dd");
+            // TODO check for null?
+            int selectedWorker = Convert.ToInt32(reportFilters[0]);
+            int selectedStatus = Convert.ToInt32(reportFilters[1]);
+
+            Report report = new Report(selectedWorker, selectedStatus);
+
+            return View(report);
+        }
+    }
+}
